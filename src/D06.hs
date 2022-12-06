2c647d2b4a790fc5b79d53dce2db38f051ac126a
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2022-12-06 11:13:40 -0500

    Add error message to Day 6 solution

--- src/D06.hs
+++ src/D06.hs
@@ -12,6 +12,6 @@ d06 :: FilePath -> Text -> Answers
 d06 _ txt = both ($ unpack txt) (getMarkerPos 4, getMarkerPos 14)
 
 getMarkerPos :: MarkerSize -> String -> String
-getMarkerPos mSize = either show show . foldM go (0 :: Int, "") where
+getMarkerPos mSize = either show (const "Marker not found!") . foldM go (0 :: Int, "") where
   go (n, cs) c = let cs' = take mSize (c:cs) in
     if S.size (S.fromList cs') == mSize then Left (n + 1) else Right (n + 1, cs')
