c6f3fb586c016dfc8f1bcf501018a7c932c534dc
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2022-12-10 15:33:59 -0500

    Add stateful parser with validation to Day 8 solution

--- src/D08.hs
+++ src/D08.hs
@@ -1,60 +1,73 @@
-module D08 where
+{-# LANGUAGE FlexibleContexts #-}
 
-import Utils ( Answers, Parser, to2DArray, both, applyWhen, prepAnswers )
+module D08 ( d08 ) where
+
+import Utils ( Answers, StateParser, both, applyWhen, toEither, fromEither, prepAnswersS )
 
 import Data.Char ( digitToInt )
-import GHC.Arr ( Array, (!), bounds, indices )
+import GHC.Arr ( Array, (!), array, bounds, indices )
 import Text.Megaparsec.Char ( digitChar, newline )
 import Control.Monad ( foldM )
+import Text.Megaparsec ( count, eof )
+import Relude.Extra (dup)
 
-type Tree = (Int, Int)
-type Height = Int
-type SightLine = [Height]
-type ViewDist = Int
+type Height       = Int
+type SightLine    = [Height]
+type Tree         = (Int, Int)
+type Forest       = Array Tree Height
 type VisibleTrees = Int
-type ScenicScore = Int
-type Forest = Array Tree Height
+type ScenicScore  = Int
 
+data ForestState = FState { getCurrIdx :: Tree
+                          , getCurrForest :: [(Tree, Height)]
+                          } deriving Show
+
+-- *** SOLUTION *** --
 d08 :: FilePath -> Text -> Answers
-d08 = prepAnswers solve gridP
-
-solve :: Forest -> Answers
-solve forest = both show . foldr (scoreTree forest) (0, 0) $ indices forest
-
-tf :: Forest
-tf = to2DArray [[3,0,3,7,3]
-               ,[2,5,5,1,2]
-               ,[6,5,3,3,2]
-               ,[3,3,5,4,9]
-               ,[3,5,3,9,0]]
+d08 = prepAnswersS solve forestP (FState (0, 0) []) -- initial forest state
+  where solve forest = both show . foldr (scoreTree forest) (0, 0) $ indices forest
 
 scoreTree :: Forest -> Tree -> (VisibleTrees, ScenicScore) -> (VisibleTrees, ScenicScore)
-scoreTree forest tree (vis, scenic) = ( applyWhen isVis (+1) vis
+scoreTree forest tree (vis, scenic) = ( applyWhen isVis (+ 1) vis
                                       , applyWhen (not $ any null slines) (max scenic') scenic)
   where
     slines = getSightLines forest tree
     -- Part 1: check visibility
     isVis = null slines || not (all (any (>= forest ! tree)) slines)
     -- Part 2: calculate scenic score (prod. of all view dists. for the tree's sight lines)
-      -- if final view distance is a Right value, subtract 1 to remove the edge
-    scenic' = getProduct $ foldMap (pure . either id rmvEdge . foldM procVDist 1) slines
-    procVDist vd h = if h < forest ! tree then Right $ vd + 1 else Left vd
-    rmvEdge = subtract 1
+    scenic' = getProduct $ foldMap (pure . fromEither . foldM procVDist 0) slines
+    procVDist vd h = toEither (h < forest ! tree) (vd + 1)
 
 getSightLines :: Forest -> Tree -> [SightLine]
-getSightLines arr (x, y)
+getSightLines forest (x, y)
   | x `elem` [x0, x'] || y `elem` [y0, y'] = [] -- skip edge trees
-  | otherwise = [us, ls, rs, ds]
-  where
-    ((x0, y0), (x', y')) = bounds arr
-    getHeights = zipWith (curry (arr !))
-    us = getHeights (repeat x) [y - 1, y - 2 .. y0]
-    ls = getHeights [x - 1, x - 2 .. x0] (repeat y)
-    rs = getHeights [x + 1 .. x'] (repeat y)
-    ds = getHeights (repeat x) [y + 1 .. y']
+  | otherwise = map (uncurry (zipWith (curry (forest !)))) -- create lines of indices & get heights
+      [ (repeat x, [y - 1, y - 2 .. y0]) -- trees above
+      , ([x - 1, x - 2 .. x0], repeat y) -- trees to left
+      , ([x + 1 .. x'], repeat y) -- trees to right
+      , (repeat x, [y + 1 .. y']) ] -- trees below
+  where ((x0, y0), (x', y')) = bounds forest
 
 -- *** PARSER *** --
-gridP :: Parser Forest
-gridP = do
-  ds <- many (many digitChar <* newline)
-  pure . to2DArray $ map (map digitToInt) ds
+forestP :: StateParser ForestState Forest
+forestP = do
+  -- parse first row to determine row length
+  (rowLength, _) <- (some heightP <* newline) >> getCurrIdx <$> get
+  -- ensure equal number of rows and columns
+  _ <- nextRow >> count (rowLength - 1) (rowP rowLength) <* eof
+  FState _ f@((uBound, _):_) <- get
+  pure $ array (dup 0, uBound) f
+
+heightP :: StateParser ForestState ()
+heightP = do
+  t <- digitToInt <$> digitChar
+  modify (addTree t) -- update the X index
+  where addTree h (FState idx forest) = FState { getCurrIdx    = first (+ 1 ) idx
+                                               , getCurrForest = (idx, h) : forest }
+
+rowP :: Int -> StateParser ForestState ()
+rowP n = (count n heightP <* newline) >> nextRow
+-- reset X index and increment Y index to prepare for new row
+
+nextRow :: (Monad m, MonadState ForestState m) => m ()
+nextRow = modify (\fState -> fState { getCurrIdx = bimap (const 0) (+ 1) (getCurrIdx fState)})
--- src/Utils.hs
+++ src/Utils.hs
@@ -2,6 +2,7 @@ module Utils where
 
 import Text.Megaparsec (
     Parsec
+  , ParsecT
   , runParser
   , errorBundlePretty
   , ParseErrorBundle
@@ -9,22 +10,39 @@ import Text.Megaparsec (
   , TraversableStream
   , ShowErrorComponent
   )
-import Relude.Extra (dup)
-import Text.Megaparsec.Char (char)
-import GHC.Arr (Array, array)
+import Relude.Extra ( dup )
+import Text.Megaparsec.Char ( char )
 
-type Parser = Parsec Void Text
-type Answer = String
-type Answers = (String, String)
+type Parser        = Parsec Void Text
+type StateParser s = StateT s (ParsecT Void Text Identity)
+type Answer        = String
+type Answers       = (String, String)
 
+-- *** ANSWER UTILS *** --
 prepAnswers :: (a -> Answers) -> Parser a -> FilePath -> Text -> Answers
 prepAnswers f p fp t = case runParser p fp t of
   Right x -> f x
   Left e  -> (errorBundlePretty e, "")
 
+prepAnswersS :: (a -> Answers) -> StateParser s a -> s -> FilePath -> Text -> Answers
+prepAnswersS f sp s = prepAnswers f (evalStateT sp s)
+
 prepAnswers2 :: (a -> Answer) -> (a -> Answer) -> Parser a -> FilePath -> Text -> Answers
 prepAnswers2 f g = prepAnswers (bimap f g . dup)
 
+sumAnswers :: (a -> Int) -> (a -> Int) -> [a] -> Answers
+sumAnswers f g = both show . foldr (\x -> bimap (+ f x) (+ g x)) (0, 0)
+
+-- *** PARSER UTILS *** --
+optSpaceP :: Parser ()
+optSpaceP = void $ optional (char ' ')
+
+-- converts error type of parse result to String for composability
+convertError :: (VisualStream s, TraversableStream s, ShowErrorComponent e)
+  => Either (ParseErrorBundle s e) a -> Either String a
+convertError = first errorBundlePretty
+
+-- *** MISC. UTILS *** --
 both :: (a -> b) -> (a, a) -> (b, b)
 both f (x, y) = (f x, f y)
 
@@ -33,26 +51,16 @@ maybeToEither e mx = case mx of
   Just x -> Right x
   Nothing -> Left e
 
-sumAnswers :: (a -> Int) -> (a -> Int) -> [a] -> Answers
-sumAnswers f g = both show . foldr (\x -> bimap (+ f x) (+ g x)) (0, 0)
-
--- converts error type of parse result to String for composability
-convertError :: (VisualStream s, TraversableStream s, ShowErrorComponent e)
-  => Either (ParseErrorBundle s e) a -> Either String a
-convertError = first errorBundlePretty
-
 safeHead :: [a] -> Maybe a
 safeHead (x:_) = Just x
 safeHead [] = Nothing
-
-optSpaceP :: Parser ()
-optSpaceP = void $ optional (char ' ')
 
 applyWhen :: Bool -> (a -> a) -> a -> a
 applyWhen True f x = f x
 applyWhen _    _ x = x
 
-to2DArray :: [[a]] -> Array (Int, Int) a
-to2DArray grid@(r:_) = let h = length grid - 1; w = length r - 1 in
-  array ((0,0), (w,h)) [((x,y), v) | (y, row) <- zip [0..] grid, (x, v) <- zip [0..] row]
-to2DArray [] = array ((0, 0), (-1, -1)) []
+fromEither :: Either a a -> a
+fromEither = either id id
+
+toEither :: Bool -> a -> Either a a
+toEither b x = if b then Right x else Left x
